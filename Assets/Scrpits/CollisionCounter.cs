158442f863cc12cad543dab66d045fc07ea5d21f
SmallShorty <46626284+SmallShorty@users.noreply.github.com>
2023-10-17 18:10:28 +0300

    sem2

---
+++ Assets/Scrpits/CollisionCounter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionCounter : MonoBehaviour
+{
+    public int counter = 10;
+
+    void Start()
+    {
+        GetComponent<Text>().text = counter.ToString();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        counter--;
+        GetComponent<Text>().text = counter.ToString();
+    }
+}
